de5caa07b29e4f08e26b2fa3cb017f16a59e94ab
Путько Николай <reg@omni-k.ru>
2022-02-24 22:39:13 +0300

    v0.1

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/', methods=['GET'])
+def index():
+    #fulldoc = int(request.args.get('fulldoc'))
+    slide = int(request.args.get('slide'))
+    print(slide)
+    #if fulldoc == 1:
+    #    return render_template('fulldoc1.html')
+    #if fulldoc == 2:
+    #    return render_template('fulldoc2.html')
+    if 1 <= slide <= 8:
+        return render_template(f'slide_{slide}.html')
+
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
